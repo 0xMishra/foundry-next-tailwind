3d1788b34445ff7c71f0bcbd02bacbb516467625
mishra811 <mishrakundan073@gmail.com>
2022-07-14 20:55:53 +0530

    added tailwind

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
 
 contract Contract {}
